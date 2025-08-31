1ecc339ed9e781c93d861ec34a531b852d6bf4b4
devops <jei7811@gmail.com>
2025-08-31 01:23:57 +0000

    [ADD] TestCase - PostTest.java

--- src/test/java/com/jei7811/guestbook/PostTest.java
+++ src/test/java/com/jei7811/guestbook/PostTest.java
@@ -1,4 +1,4 @@
-package com.jei7811.guestbook;
+package com.jadecross.guestbook;
  import static org.junit.jupiter.api.Assertions.assertEquals;
  import org.junit.jupiter.api.Test;
  class PostTest {
